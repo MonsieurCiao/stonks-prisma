93a9f7c9d6a336ac7b02fcfdb6f8a71cf1dfb524
MonsieurCiao <luchaokas@gmail.com>
2025-07-14 22:03:52 +0200

    bitti compili 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+
 
 export function middleware() {
   return new NextResponse('Site under maintenance', { status: 503 });
